c698c34e4a09d01f7ce789b46fefecec9593d6e2
sophia-masomo <sophimari2004@gmail.com>
2023-08-01 15:09:23 -0300

    relacionamento paciente contato

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Paciente extends Model
 {
@@ -18,4 +19,9 @@ class Paciente extends Model
         'data_nasc',
         'cpf',
     ];
+
+    public function contatos(): HasMany
+    {
+        return $this->hasMany(Contato::class, 'id_paciente', 'id');
+    }
 }
