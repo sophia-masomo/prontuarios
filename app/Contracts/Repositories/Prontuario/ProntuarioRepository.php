60fd391e425f0c0998fdda64ec3635b439f4c11e
sophia-masomo <sophimari2004@gmail.com>
2023-08-02 17:01:26 -0300

    métodos prontuário

--- app/Contracts/Repositories/Prontuario/ProntuarioRepository.php
+++ app/Contracts/Repositories/Prontuario/ProntuarioRepository.php
@@ -2,11 +2,12 @@
 
 namespace App\Contracts\Repositories\Prontuario;
 
-//use App\Interfaces\ProntuarioRepositoryInterface;
+use App\Interfaces\ProntuarioRepositoryInterface;
 use App\Models\Prontuario;
 
 class ProntuarioRepository implements ProntuarioRepositoryInterface
 {
+        //salvar
     public function store(Request $request) {
         $prontuario = new Prontuario;
 
@@ -15,19 +16,28 @@ class ProntuarioRepository implements ProntuarioRepositoryInterface
         $prontuario->dt_criacao = $request->dt_criacao;
         $prontuario->dt_atualizacao = $request->dt_atualizacao;
         $prontuario->dt_exclusao = $request->dt_exclusao;
+        $prontuario->id_paciente = $request->id_paciente;
 
         $prontuario->save();
     }
 
-    /*public function update() {
-
+        //atualizar
+    public function update($id, array $dado)
+    {
+        $prontuario = Prontuario::findOrFail($id);
+        $prontuario->update($dado);
+        return $prontuario;
     }
 
-    public function porld() {
-        
+        //porld
+    public function porld($id)
+    {
+        return Prontuario::findOrFail($id);
     }
 
-    public function remove() {
-
-    }*/
+        //remover
+    public function softDelete($id)
+    {
+        $prontuario = Prontuario::find($id);
+        $prontuario->delete();}
 } 
